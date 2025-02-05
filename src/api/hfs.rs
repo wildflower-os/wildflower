ee7a4db1f694fc865c64a27289bc3656975bf2f8
Zander <zander@zanderlewis.dev>
2025-02-05 11:26:55 -0500

    Fix bounds check by using wildcard

--- src/api/hfs.rs
+++ src/api/hfs.rs
@@ -1,12 +1,12 @@
 use crate::api::process::ExitCode;
 
 pub fn check_hfs_bounds(arg: &str) -> Result<(), ExitCode> {
-    if arg == "/hfs" || arg == "/hfs/" {
+    if arg == "/hfs" || arg == "/hfs/" || arg.starts_with("/hfs/") {
         #[cfg(not(test))]
         error!("Permission denied. HFS is non-accessible by users.");
         return Err(ExitCode::Failure);
     }
-    if crate::sys::process::dir() == "/" && (arg == "hfs" || arg == "hfs/") {
+    if crate::sys::process::dir() == "/" && (arg == "hfs" || arg == "hfs/" || arg.starts_with("hfs/")) {
         #[cfg(not(test))]
         error!("Permission denied. HFS is non-accessible by users.");
         return Err(ExitCode::Failure);
